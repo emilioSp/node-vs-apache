cf1f4cd066989650383f1e0e395732bc88cce58a
espatola <emilio.spatola@gmail.com>
2019-12-17 00:25:12 +0100

    Update test with a simulated io operation

---
+++ node/server.js
@@ -0,0 +1,34 @@
+const http = require('http');
+const fs = require('fs');
+const url = require('url');
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+const server = http.createServer( async (req, res) => {
+  const requestUrl = url.parse(req.url);
+  const path = requestUrl.pathname;
+  switch (path) {
+    case '/static':
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      const stream = fs.createReadStream('lorem_ipsum.html');
+      stream.pipe(res);
+      break;
+    case '/simulated-io':
+      res.writeHead(200, {'Content-Type': 'application/json'});
+      await sleep(100);
+      res.end('OK');
+      break;
+    case '/cpu-intensive':
+      res.writeHead(200, {'Content-Type': 'application/json'});
+      // TODO: write cpu intensive task
+      res.end('OK');
+      break;
+    default:
+      res.writeHead(404, 'route not defined');
+      res.end();
+  }
+});
+
+server.listen(8000);
